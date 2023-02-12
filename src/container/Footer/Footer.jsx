5a28022d61afb5ec7988b8902497498b98cf8a73
Adham Elgohary <adham.elgohary.12@gmail.com>
2023-02-12 22:18:24 +0200

    Update Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -19,9 +19,9 @@ const Footer = () => (
       </div>
 
       <div className="app__footer-links_logo">
-        <img src={images.gericht} alt="footer_logo" />
+        <src={images.gericht} alt="footer_logo" />
         <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
-        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} alt="spoon" />
         <div className="app__footer-links_icons">
           <FiFacebook />
           <FiTwitter />
